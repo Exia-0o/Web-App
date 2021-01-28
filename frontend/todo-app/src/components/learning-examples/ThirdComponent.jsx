c77c9a3609f32a1a371c961155ce963e4968d0de
Xinghang Li <michaelli940321@gmail.com>
2021-01-27 17:45:12 -0800

    finish section 1

---
+++ frontend/todo-app/src/components/learning-examples/ThirdComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// Function Component
+function ThirdComponent() {
+    return (
+      <div className="ThirdComponent">
+        Third Component
+      </div>
+    )
+  }
+
+export default ThirdComponent;
